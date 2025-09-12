df461bb09ee88d4b1438d6e87485ea14d147ff16
aloenne-qery <alf.lonne@qery.no>
2025-09-12 10:31:40 +0200

    fix starting year

--- Eurostat.py
+++ Eurostat.py
@@ -77,12 +77,12 @@ df_new.index = [f"{flag_codes[country]} {country}" for country in df_new.index]
 df_new.to_csv('data/Eurostat_Life_Expectancy_Country_Males_Latest_Year_Flags.csv', index=True)
 
 #Life expectancy latest year over time by country
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/demo_mlexpec?format=JSON&lang=en&freq=A&unit=YR&sex=T&age=Y_LT1&geo=EU27_2020&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&lastTimePeriod=22')
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/demo_mlexpec?format=JSON&lang=en&freq=A&unit=YR&sex=T&age=Y_LT1&geo=EU27_2020&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&lastTimePeriod=23')
 df = dataset.write('dataframe')
 df.replace(rename_dict, inplace=True)
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
-df_new['Change since 2003'] = df_new['2024'] - df_new['2003']
-df_new['Change since 2003 in %'] = (df_new['2024'] - df_new['2003'])/df_new['2003']*100
+df_new['Change since 2002'] = df_new['2024'] - df_new['2002']
+df_new['Change since 2002 in %'] = (df_new['2024'] - df_new['2002'])/df_new['2002']*100
 df_new['Change since 2019'] = df_new['2024'] - df_new['2019']
 df_new['Change since 2019 in %'] = (df_new['2024'] - df_new['2019'])/(df_new['2019'])*100
 df_new = df_new[df_new['2024'].notna()]
